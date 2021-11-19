e088bc41ea9035e066c5645d3f17c7ac99510b9d
Leonardo Hernández <leonardo.hernandez@genesys.cl>
2021-11-19 18:14:36 -0500

    Trabajando con ExceptionTest.cs para validar manejo de errores con NUnit

---
+++ ArrayList/ExceptionTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ArrayList
+{
+    [TestFixture]
+    public class ExceptionTest
+    {
+        [Test]
+        public void DivisionPorCeroLanzaZeroDiviionError()
+        {
+            Func<int, int, int> dividir = (int dividendo, int divisor) => dividendo / divisor;
+            Assert.Throws<DivideByZeroException>(() => dividir(1, 0));
+        }
+
+        [Test]
+        public void AccesoFueraDeRangoLanzaIndexOutOfRangeException()
+        {
+            int[] items = new[] {1, 2};
+            Assert.Throws<IndexOutOfRangeException>(() => items[3].ToString());
+        }
+
+
+        [Test]
+        public void InvocarSobreUnMetodoDeUnaReferenciaQueEsNullLanzaNullReferenceException()
+        {
+            System.Collections.ArrayList arr = null;
+            Assert.Throws<NullReferenceException>(() => arr.Clear());
+        }
+    }
+}
---
+++ ArrayList/StackTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ArrayList
+{
+    public class StackTest
+    {
+        [Test]
+        public void UltimoEntradoPrimeroSalido()
+        {
+            var stack = new Stack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+
+            Assert.That(stack.Pop(), Is.EqualTo(4));
+            Assert.That(stack.Pop(), Is.EqualTo(3));
+            Assert.That(stack.Pop(), Is.EqualTo(2));
+            Assert.That(stack.Pop(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void LanzaInvalidOperationExceptionAlSacarCuandoEstaVacio()
+        {
+            var stack = new Stack<int>();
+            Assert.Throws<InvalidOperationException>(() => stack.Pop());
+        }
+
+        [Test]
+        public void SeCreaVacio()
+        {
+            var stack = new Stack<int>();
+            Assert.IsEmpty(stack);
+        }
+    }
+}
